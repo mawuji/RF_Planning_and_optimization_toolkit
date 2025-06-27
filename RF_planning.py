77e89958f4dda146d62e4c3f6a78c1a831ef54f7
mawuji <61087352+mawuji@users.noreply.github.com>
2025-06-27 16:12:25 +0000

    Update RF_planning.py

--- RF_planning.py
+++ RF_planning.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import folium
+
 from streamlit_folium import st_folium
 from folium.plugins import MarkerCluster
 import matplotlib.pyplot as plt
